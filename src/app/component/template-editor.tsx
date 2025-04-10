e0f34a8206f61d09c23532d1f8fd37bdc30466a2
Stenin K George <steninkgeorge007@gmail.com>
2025-04-10 18:21:20 +0530

    uncomment

--- src/app/component/template-editor.tsx
+++ src/app/component/template-editor.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 
 import { Editor, EditorContent } from '@tiptap/react';
+import { TextBubbleMenu } from './bubble/bubble-menu';
 // import { TextBubbleMenu } from './bubble/bubble-menu';
 
 interface TemplateEditorProps {
@@ -12,7 +13,7 @@ export const TemplateEditor = ({ editor }: TemplateEditorProps) => {
   return (
     <div>
       <EditorContent editor={editor} />
-      {/* <TextBubbleMenu editor={editor} /> */}
+      <TextBubbleMenu editor={editor} />
     </div>
   );
 };
