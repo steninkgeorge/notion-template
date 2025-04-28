006997617a81deb3953d7264d41af84297967ea2
Stenin K George <steninkgeorge007@gmail.com>
2025-04-28 23:41:31 +0530

    title node and custom placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { TocOverlay } from '@/tiptap-extensions/heading/table-of-content';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center min-h-screen bg-white">
       <div className="toolbar-container w-full">
         <Toolbar />
       </div>
